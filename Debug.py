9a665130cc0bfc74fdbb0b4a6d4ccbe5cc0b23b5
Axel Grönland <gronland@kth.se>
2021-10-23 15:21:42 +0200

    added outlines for autoencoder

--- Debug.py
+++ Debug.py
@@ -1,4 +1,12 @@
 import torch
 import MLlib
+import numpy as np
 
+
+
+sequence = np.arange(100)
+#sequence[40:45] = 0
+#sequence[95:100] = 0
+
+net = MLlib.Models.AutoEncoder(1,2)
 
--- MLlib/Models.py
+++ MLlib/Models.py
@@ -1,4 +1,37 @@
+import torch
+from torch._C import INSERT_FOLD_PREPACK_OPS
+import torch.nn as nn
 
 
-class Model():
-    pass
+class Encoder(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(Encoder, self).__init__()
+        self._in_size = in_size
+        self._out_size = out_size
+
+    
+    def forward(self, x):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(Decoder, self).__init__()
+        self._in_size = in_size
+        self._out_size = out_size
+    
+    def forward(self, z):
+        pass
+
+class AutoEncoder(nn.Module):
+    def __init__(self, in_size, hidden_size):
+        super(AutoEncoder, self).__init__()
+        self._in_size = in_size
+        self._hidden_size = hidden_size
+
+        self.encoder = Encoder(in_size, hidden_size)
+        self.decoder = Decoder(hidden_size, in_size)
+
+    def forward(self, x):
+        out = self.encoder(x)
+        out = self.decoder(out)
+        return out
