adf63b167721bebcb6596d0f27720d97dc80546b
Axel Grönland <gronland@kth.se>
2021-10-23 20:31:57 +0200

    finalized the model2

--- Debug.py
+++ Debug.py
@@ -9,12 +9,40 @@ from copy import deepcopy
 import MLlib.DSP as gd 
 
 
+net = MLlib.Models.AutoEncoder()
+trainer = Trainer(net)
+
+def noise(x, p=0.01):
+    mask = torch.rand(x.shape) < p
+    out = deepcopy(x)
+    out[mask] = 1 - x[mask]
+    return out
+
+def remove_chunk(x, start, T, end=10000):
+    out = deepcopy(x)
+    end = min(end, start+T)
+    out[start:end] = 0
+    return out
+
 
 for x in gd.get_data('./Datasets/0/'): 
-    print(x.shape[0])
+    print(x.shape)
+    x = torch.Tensor(x)
+    x_slice = x[:10000].T
+
+    x_input = x_slice.view((1, 88, 10000))
+
+    x_disturbed = noise(x_input)
+    x_with_hole = remove_chunk(x, 5000, 1000)
+
+    y = net(x_input)
+    error = trainer.train(x_input, 10, x_disturbed)
+    plt.plot(error)
+    plt.show()
     #print(gd.arry2mid(x))  
     # dada1 = x
     # data2 = gd.arry2mid(data1)
     # print(data1)
     # print(data2)
     
+    break
--- MLlib/Trainer.py
+++ MLlib/Trainer.py
@@ -10,17 +10,24 @@ class Trainer():
 
         
 
-    def train(self, data, epochs, data_distorted):
+    def train(self, data, epochs, data_distorted, data_chunk_removed):
         reconstruction_error = []
+        denoising_error = []
         for ep in range(epochs):
             preds = self.model.forward(data)
-            loss = self._loss_func(preds, data)
+            loss = self._loss_func(preds, data)/(data.shape[0]*data.shape[1])
             loss.backward()
+            print(loss)
+
             self.optim.step()
             self.optim.zero_grad()
 
             # print the reconstruction error
-            data_reconstructed = self.model.forward(data_distorted)
-            reconstruction_loss = float(torch.norm(data-data_reconstructed))
+            data_denoised = self.model.forward(data_distorted)
+            data_reconstructed = self.model.forward(data_chunk_removed)
+            reconstruction_loss = float(torch.norm(data-data_reconstructed)**2/(data.shape[0]*data.shape[1]))
+            noise_loss = float(torch.norm(data - data_denoised)**2/(data.shape[0]*data.shape[1]))
+            
             reconstruction_error.append(reconstruction_loss)
+            denoising_error.append(noise_loss)
         return reconstruction_error
