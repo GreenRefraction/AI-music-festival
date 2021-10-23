93eafdb1025ea69a87e0c60f459cb8ebd0eee850
Axel Grönland <gronland@kth.se>
2021-10-23 17:28:10 +0200

    now we have a midi to numpy array

--- Debug.py
+++ Debug.py
@@ -1,14 +1,26 @@
 import torch
 import MLlib
 import numpy as np
-
+import torch.nn as nn
 from MLlib.Trainer import Trainer
+import matplotlib.pyplot as plt
 
 
 
-sequence = np.arange(100)
-#sequence[40:45] = 0
-#sequence[95:100] = 0
+X = MLlib.DSP.get_data()
 
-net = MLlib.Models.AutoEncoder(1,2)
+
+net = MLlib.Models.AutoEncoder(2)
+
 trainer = Trainer(net)
+trainer.train(sequence, 1000)
+
+preds = torch.Tensor(sequence)
+seq2 = torch.Tensor([np.sin(np.linspace(0,2*np.pi, 100)),
+                np.cos(np.linspace(0,2*np.pi, 100))]).T
+
+seq2_rec = net(seq2)
+seq2_rec = seq2_rec.detach().numpy()
+plt.plot(seq2[:, 0], 'r')
+plt.plot(seq2_rec[:, 0], 'b')
+plt.show()
--- MLlib/Trainer.py
+++ MLlib/Trainer.py
@@ -4,17 +4,18 @@ import torch.optim as optim
 
 class Trainer():
     def __init__(self, model:nn.Module) -> None:
-        self.optim = optim.SGD(model.parameters(), 2e-3)
+        self.optim = optim.SGD(model.parameters(), 2e-2)
         self.model:nn.Module = model
-        self._loss_func = None
+        self._loss_func = nn.MSELoss()
 
         
 
     def train(self, data, epochs):
-        
         for ep in range(epochs):
             preds = self.model.forward(data)
-            loss = self._loss_func(preds)
+            loss = self._loss_func(preds, data)
+            print(float(loss))
             loss.backward()
-
-        pass
+            self.optim.step()
+            self.optim.zero_grad()
+        
