511b7d3f53d439ae98ebefe07ce23e8717de1965
Axel Grönland <gronland@kth.se>
2021-10-23 16:05:47 +0200

    added some barebones code in trainer

--- Debug.py
+++ Debug.py
@@ -2,6 +2,8 @@ import torch
 import MLlib
 import numpy as np
 
+from MLlib.Trainer import Trainer
+
 
 
 sequence = np.arange(100)
@@ -9,4 +11,4 @@ sequence = np.arange(100)
 #sequence[95:100] = 0
 
 net = MLlib.Models.AutoEncoder(1,2)
-
+trainer = Trainer(net)
--- MLlib/Trainer.py
+++ MLlib/Trainer.py
@@ -1,4 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 class Trainer():
-    def train():
+    def __init__(self, model:nn.Module) -> None:
+        self.optim = optim.SGD(model.parameters(), 2e-3)
+        self.model:nn.Module = model
+        self._loss_func = None
+
+        
+
+    def train(self, data, epochs):
+        
+        for ep in range(epochs):
+            preds = self.model.forward(data)
+            loss = self._loss_func(preds)
+            loss.backward()
+
         pass
