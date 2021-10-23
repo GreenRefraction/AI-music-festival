903b1f200525924342c88d9da1fe0c63a3890d8e
Axel Grönland <gronland@kth.se>
2021-10-23 13:26:04 +0200

    added outlines of the project

---
+++ MLlib/DSP.py
@@ -0,0 +1,7 @@
+# Here we create functions that retrieve the data
+# get and parse MIDI files from million song dataset
+# https://colinraffel.com/projects/lmd/?fbclid=IwAR3uI_G82wcYmWVEFkkHUQe5CzdOkXlE8fvKpQLnVawNJ8HIEYHXnJVoUps#get
+
+
+def get_data():
+    pass
