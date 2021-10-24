b25d46b15b9ccbd3e81f432242646797589f0784
aacraf <aacraaf@gmail.com>
2021-10-24 10:24:32 +0200

    frontend end

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-from MLlib.DSP import get_data
+from flask import Flask, render_template, request, send_file, send_from_directory, redirect, url_for
+from MLlib.DSP import get_data, arry2mid
+from MLlib.Merger import merge_midi_matrix
 import time, os 
 app = Flask(__name__)
 
@@ -20,10 +21,28 @@ def enter_data():
         file1.save(os.path.join(upload_folder, file1.filename))
         file2.save(os.path.join(upload_folder, file2.filename))
         # get data from files
-        for x in get_data('./uploads/'):
-            print(x)
+        vec1 = next((x for i,x in enumerate(get_data('./uploads/')) if i==0), None)
+        vec2 = next((x for i,x in enumerate(get_data('./uploads/')) if i==1), None)
+        print(vec1)
+        print(vec2)
+        arr_merged = merge_midi_matrix(vec1,vec2)
+        merged_mid = arry2mid(arr_merged)
+        merged_mid.save('./uploads/merged.mid')
         
-        
-        
-        
+        return redirect(url_for('get_files', path='merged.mid'))  
     return render_template('index.html')
+
+
+# Specify directory to download from . . .
+# 
+
+DOWNLOAD_DIRECTORY = "./uploads"
+
+@app.route('/get-files/<path:path>',methods = ['GET','POST'])
+def get_files(path):
+
+    """Download a file."""
+    try:
+        return send_from_directory(DOWNLOAD_DIRECTORY, path, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
